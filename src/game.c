e9fbd4affaa609937df6e93b9d5f73998f746735
mts0629 <soratobi93@gmail.com>
2026-02-27 16:14:11 +0900

    Make a function for printing

--- src/game.c
+++ src/game.c
@@ -11,12 +11,69 @@
 
 typedef uint8_t Card;
 
+char *get_suit(const Card *card) {
+    switch (*card & 0xf0) {
+        case 0x00:
+            return "H";
+            break;
+        case 0x10:
+            return "D";
+            break;
+        case 0x20:
+            return "C";
+            break;
+        case 0x30:
+            return "S";
+            break;
+        case 0x40:
+            return "JOK";
+            break;
+        default:
+            fprintf(stderr, "Invalid suit\n");
+            return NULL;
+            break;
+    }
+}
+
+char *get_num_str(const Card *card) {
+    if (*card == 0x40) {
+        return "";
+    }
+
+    int n = *card & 0x0f;
+    static char buf[3];
+    switch (n) {
+        case 13:
+            return "K";
+            break;
+        case 12:
+            return "Q";
+            break;
+        case 11:
+            return "J";
+            break;
+        case 1:
+            return "A";
+            break;
+        default:
+            snprintf(buf, sizeof(buf), "%d", n);
+            return buf;
+            break;
+    }
+}
+
+char *get_card_str(const Card *card) {
+    static char buf[5];
+    snprintf(buf, sizeof(buf), "%s:%s", get_suit(card), get_num_str(card));
+    return buf;
+}
+
 int main(void) {
     // Init a deck
     Card deck[4 * 13 + 2];
     for (uint8_t i = 0; i < 4; i++) {
         uint8_t s = i << 4;
-        for (uint8_t j = 0; j < 13; j++) {
+        for (uint8_t j = 1; j <= 13; j++) {
             deck[i * 13 + j] = s + j;
         }
     }
@@ -40,47 +97,7 @@ int main(void) {
 
     // Show cards
     for (int i = 0; i < 5; i++) {
-        // Print suit
-        switch (cards[i] & 0xf0) {
-            case 0x00:
-                printf("H");
-                break;
-            case 0x10:
-                printf("D");
-                break;
-            case 0x20:
-                printf("C");
-                break;
-            case 0x30:
-                printf("S");
-                break;
-            case 0x40:
-                printf("JOK ");
-                continue;
-                break;
-            default:
-                fprintf(stderr, "Invalid card\n");
-                exit(1);
-                break;
-        }
-        int n = cards[i] & 0x0f;
-        switch (n) {
-            case 12:
-                printf(":K ");
-                break;
-            case 11:
-                printf(":Q ");
-                break;
-            case 10:
-                printf(":J ");
-                break;
-            case 0:
-                printf(":A ");
-                break;
-            default:
-                printf(":%d ", (n + 1));
-                break;
-        }
+        printf("%s ", get_card_str(&cards[i]));
     }
     printf("\n");
 
