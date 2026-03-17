39efaf8e370fd19bcd7869e5562a4bed7d9b1e77
mts0629 <soratobi93@gmail.com>
2026-03-17 23:59:59 +0900

    Judge the game

--- src/game.c
+++ src/game.c
@@ -2,19 +2,75 @@
 
 #include "card.h"
 
+void judge(const Status *player, const Status *com) {
+    if (player->hand > com->hand) {
+        printf("You win!\n");
+        return;
+    } else if (player->hand < com->hand) {
+        printf("You lose...\n");
+        return;
+    } else {
+        if (player->rank[0] > com->rank[0]) {
+            printf("You win!\n");
+            return;
+        } else if (player->rank[0] < com->rank[0]) {
+            printf("You lose...\n");
+            return;
+        } else {
+            if (player->hand == TWO_PAIR) {
+                if (player->rank[1] > com->rank[1]) {
+                    printf("You win!\n");
+                    return;
+                } else if (player->rank[1] < com->rank[1]) {
+                    printf("You lose...\n");
+                    return;
+                }
+            } else {
+                int i = 0;
+                while (player->kicker[i] != 0) {
+                    if (player->kicker[i] > com->kicker[i]) {
+                        printf("You win!\n");
+                        return;
+                    } else if (player->kicker[i] < com->kicker[i]) {
+                        printf("You lose...\n");
+                        return;
+                    }
+                    i++;
+                }
+            }
+        }
+
+        printf("Draw\n");
+    }
+}
+
 int main(void) {
     init_deck();
 
     shuffle_deck(100);
 
-    Card *hand = draw_hand();
-    hand = sort_cards(hand);
+    Card *player_hand = draw_hand();
+    player_hand = sort_cards(player_hand);
 
-    Status status;
-    get_status(&status, hand);
+    Card *com_hand = draw_hand();
+    com_hand = sort_cards(com_hand);
 
-    print_cards(hand);
-    print_status(&status);
+    Status player_status;
+    get_status(&player_status, player_hand);
+
+    printf("----- Your card -----\n");
+    print_cards(player_hand);
+    print_status(&player_status);
+
+    Status com_status;
+    get_status(&com_status, com_hand);
+
+    printf("----- COM card ------\n");
+    print_cards(com_hand);
+    print_status(&com_status);
+    printf("---------------------\n");
+
+    judge(&player_status, &com_status);
 
     fini_deck();
 
